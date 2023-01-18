c964f5f3a0bf2433f8ab9e1f3ec0b636934c3758
themisAnagno <thmanagnostopoulos@gmail.com>
2023-01-18 14:17:38 +0000

    Moved creds to remote server

--- bin/validate.sh
+++ bin/validate.sh
@@ -4,5 +4,5 @@ for i in $(ls openstack);
 do
     echo "*** Validating cloud: ${i} ***"
     terraform -chdir="./openstack/${i}" init
-    terraform -chdir="./openstack/${i}" init
+    terraform -chdir="./openstack/${i}" validate
 done
