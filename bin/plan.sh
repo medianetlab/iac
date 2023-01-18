3fa0f856947fbc4bd53a6ec3bd655c6e09810236
themisAnagno <thmanagnostopoulos@gmail.com>
2023-01-18 14:22:17 +0000

    Updated scripts

--- bin/plan.sh
+++ bin/plan.sh
@@ -3,5 +3,5 @@
 for i in $(ls openstack);
 do
     echo "*** Plan cloud: ${i} ***"
-    terraform -chdir="./openstack/${i}" plan -var-file=instances -var-file=/home/ubuntu/.iac/creds/terraform.tfvars
+    terraform -chdir="./openstack/${i}" plan -var-file=instances -var-file=/home/${USER}/.iac/creds/terraform.tfvars
 done
