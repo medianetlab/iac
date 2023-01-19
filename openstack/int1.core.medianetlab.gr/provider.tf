d0e5b48ff6b06f8d129fa6d4c551b7bf4bcd2919
themisAnagno <thmanagnostopoulos@gmail.com>
2023-01-19 09:56:38 +0000

    Added int1.core openstack

---
+++ openstack/int1.core.medianetlab.gr/provider.tf
@@ -0,0 +1,35 @@
+# Define required providers
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.48.0"
+    }
+  }
+  backend "local" {
+    path = ""
+  }
+}
+
+# Terraform provider variables
+variable "user_name" {
+  type = string
+}
+
+variable "tenant_name" {
+  type = string
+}
+
+variable "password" {
+  type = string
+}
+
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_name   = var.user_name
+  tenant_name = var.tenant_name
+  password    = var.password
+  auth_url    = "http://10.161.0.2:5000/v3"
+  #  region      = "RegionOne"
+}
